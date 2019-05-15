5265fe27cb50a7566eb9de0734c3c5cdad7fcd47
Jan Scheufler <jan.scheufler@t-systems.com>
2019-05-15 17:55:58 +0200

    main.go - try to find data in output

--- src/main.go
+++ src/main.go
@@ -10,6 +10,8 @@ import (
 )
 
 func main() {
+	var ChangeNotes map[string]map[string]string
+
 	resp, err := http.Get("https://docs.otc.t-systems.com/en-us/usermanual/obs/en-us_topic_0071293550.html")
 
 	if err != nil {
@@ -22,14 +24,52 @@ func main() {
 	}
 
 	bodyAsSlice := strings.Split(string(body), "\n")
-	for index, element := range bodyAsSlice {
-		output := stripHTML(element)
-		fmt.Println(index, output)
+	var ReadingChangeNotes bool
+	var InsideTableBody bool
+	var currentKey string
+
+	// Loop over returned HTML, do not register index
+	for _, element := range bodyAsSlice {
+
+		// The heading "Change History" marks the beginning of the interesting part
+		if strings.Contains(element, ">Change History</h1>") {
+			ReadingChangeNotes = true
+			continue
+		}
+		// fmt.Println("ReadingChangeNotes is set to:", ReadingChangeNotes)
+		if ReadingChangeNotes {
+			// First occurrence of "<tbody>" after entering the Change Notes marks the beginning
+			if strings.Contains(element, "<tbody>") {
+				InsideTableBody = true
+			}
+			// First occurrence of "</tbody>" after entering the Change Notes marks the end
+			if strings.Contains(element, "</tbody>") {
+				ReadingChangeNotes = false
+				break
+			}
+		}
+
+		// fmt.Println("InsideTableBody is set to:", InsideTableBody)
+		if InsideTableBody {
+			re := regexp.MustCompile(`>[0-9]{4}-[0-9]{2}-[0-9]{2}<`)
+			if len(re.Find([]byte(element))) > 0 {
+				currentKey = (stripHTML(element))
+				ChangeNotes[currentKey] = make(map[string]string)
+				// fmt.Println(index, element)
+				// output := stripHTML(element)
+				// fmt.Println(index, output)
+			} else {
+				ChangeNotes[currentKey][stripHTML(element)] = ""
+			}
+		}
 	}
 	// fmt.Println(string(body))
+	for key, value := range ChangeNotes {
+		fmt.Println("Key:", key, "Value:", value)
+	}
 }
 
 func stripHTML(html string) string {
 	re := regexp.MustCompile(`<.+?>`)
-	return re.ReplaceAllString(html, "")
+	return strings.TrimSpace(re.ReplaceAllString(html, ""))
 }
